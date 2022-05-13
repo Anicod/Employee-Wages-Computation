b6d4c196813e8c66e32b435b59913a764c788e5e
Anicod <animeshraj735@gmail.com>
2022-05-13 23:33:21 +0530

    added employee status

--- src/empwagebuilder.java
+++ src/empwagebuilder.java
@@ -1,8 +1,14 @@
 public class empwagebuilder {
+    static final int EMP_STATUS = 1;
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Computation Program");
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == EMP_STATUS)
+            System.out.println("employee is present");
+        else
+            System.out.println("employee is absent");
     }
 }
 
