b7ebc41bce92f4e22cb093375cd2bc1e3d73b0d3
Anicod <animeshraj735@gmail.com>
2022-05-14 03:27:43 +0530

    added part time worker wages

--- src/empwagebuilder.java
+++ src/empwagebuilder.java
@@ -1,13 +1,17 @@
 public class empwagebuilder {
     static final int EMP_STATUS = 1;
+    static final int WAGES_PER_HOUR = 20
+    static final int PART_TIME_WORKER = 4;
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Computation Program");
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == EMP_STATUS)
+        if (empCheck == EMP_STATUS) {
             System.out.println("employee is present");
-        else
+            int Part_TIME_WORKER_WAGES = PART_TIME_WORKER * WAGES_PER_HOUR;
+            System.out.println("employee wages:" + Part_TIME_WORKER_WAGES);
+        } else
             System.out.println("employee is absent");
     }
 }
